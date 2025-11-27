c2d0a49176672906ee17ac4b3d9500695cd794cb
Abhiral Jain <jainabhiral7@gmail.com>
2025-11-27 14:00:36 +0530

    Update 3.py

--- 3.py
+++ 3.py
@@ -2,62 +2,62 @@
 
 ## linestyle - You can use the keyword argument linestyle, or shorter ls, to change the style of the plotted line
 
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-# ypoints = np.array([3,8,2,10])
+ypoints = np.array([3,8,2,10])
 
-# # plt.plot(ypoints ,ls = "dotted")
-# # plt.plot(ypoints , ls = "dashed")
+plt.plot(ypoints ,ls = "dotted")      ## the dotted and dashed are the types of linestyle. At one time u can use only one 
+plt.plot(ypoints , ls = "dashed")
 
-# plt.show()
+plt.show()
 
 ## linecolor - We can use the keyword argument color or the shorter c to set the color of the line
 
-# import numpy as np
-# import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
-# ypoints = np.array([3,8,2,10])
+ypoints = np.array([3,8,2,10])
 
-# # plt.plot(ypoints , c = "red")
-# # plt.plot(ypoints , color = "hotpink")
-# # plt.plot(ypoints , color = "blue")
+plt.plot(ypoints , c = "red")
+plt.plot(ypoints , color = "hotpink")    ## these are the colors used to colour line. At one time u can use only one 
+plt.plot(ypoints , color = "blue")
 
-# plt.show()
+plt.show()
 
 ###linewidth - we use the keyword argument linewidth or the lw to change the width of line
 
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
 
-# ypoint = np.array([2,9,6,8])
+ypoint = np.array([2,9,6,8])
 
-# plt.plot(ypoint ,linewidth = 11.5 )
+plt.plot(ypoint ,linewidth = 11.5 )
 
-# plt.show()
+plt.show()
 
 
 ### multiple lines
 
-# import matplotlib.pyplot as plt
-# import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
  
-# y1 = np.array([3,6,2,8])
-# y2 = np.array ([2,5,3,7])
+y1 = np.array([3,6,2,8])
+y2 = np.array ([2,5,3,7])
 
-# plt.plot(y1)
-# plt.plot(y2)
+plt.plot(y1)            ## whenever u are plotting mutiple lines, the lines of color is orange and blue (by default)
+plt.plot(y2)
 
-# plt.show()
+plt.show()
 
 
 # import matplotlib.pyplot as plt
 # import numpy as np
 
-# x1 = np.array([0, 1, 2, 3])
-# y1 = np.array([3, 8, 1, 10])
-# x2 = np.array([0, 1, 2, 3])
-# y2 = np.array([6, 2, 7, 11])
+x1 = np.array([0, 1, 2, 3])
+y1 = np.array([3, 8, 1, 10])
+x2 = np.array([0, 1, 2, 3])
+y2 = np.array([6, 2, 7, 11])
 
-# plt.plot(x1, y1, x2, y2)
-# plt.show()
+plt.plot(x1, y1, x2, y2)
+plt.show()
