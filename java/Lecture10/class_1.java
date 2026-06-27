0c4bc0a68171d0b2c07fd8afb1d2c482117ff2d3
Abhiral Jain <jainabhiral7@gmail.com>
2026-06-27 09:55:30 +0530

    Update class_1.java

--- java/Lecture10/class_1.java
+++ java/Lecture10/class_1.java
@@ -6,23 +6,23 @@ package Lecture10;
 
 // store the marks of students in the array
 
-// import java.util.*;
-// public class class_1 {
+import java.util.*;
+public class class_1 {
 
-//     public static void main(String[] args) {
-//         Scanner sc = new Scanner(System.in);
-//         int n = sc.nextInt();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-//         int marks [] = new int[3];
-//         marks[0] = 92;
-//         marks[1] = 94;
-//         marks[2] = 90;
+        int marks [] = new int[3];
+        marks[0] = 92;
+        marks[1] = 94;
+        marks[2] = 90;
 
-//         System.out.println(marks[0]);
-//         System.out.println(marks[1]);   //ye wale method mai hme ek-ek krke number print krwana hota h
-//         System.out.println(marks[2]);
-//     }
-// }
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);   //ye wale method mai hme ek-ek krke number print krwana hota h
+        System.out.println(marks[2]);
+    }
+}
 
 
 //using FOR loop - loop lgane se saare marks ek baar mai print ho jayenge
